20c204cf2123509c275ae5a481c1386d8f981e63
Rich Pauloo <richpauloo@gmail.com>
2018-09-06 17:37:21 -0700

    initial commit
    working geocoder. still needs an updated about.md page

---
+++ app/ui.R
@@ -0,0 +1,46 @@
+# Define UI for application that draws a histogram
+ui <- 
+  dashboardPage(
+    dashboardHeader(title = "Water System Locater",
+                    titleWidth = 300),
+    dashboardSidebar(
+      width = 300,
+      textInput("street", 
+                label = h4("Enter Address"), 
+                placeholder = "Street Number"),
+      textInput("city", 
+                label = "", 
+                placeholder = "City"),
+      textInput("zip", 
+                label = "", 
+                placeholder = "Zip Code"),
+      actionButton("go","Enter")
+      
+    ),
+    
+    # Body
+    dashboardBody(
+      tags$head(
+        tags$style(HTML("
+                        blockquote {
+                        padding: 10px 20px;
+                        margin: 0 0 20px;
+                        font-size: 13px;
+                        border-left: 5px solid #eee;
+                        }
+                        "))),
+      fluidRow(
+        tabBox(width = 12, height = NULL,
+               tabPanel("Map", 
+                        tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
+                        leafletOutput("map")),
+               tabPanel(title = "About", 
+                        includeMarkdown("about.md"))
+        )
+      ),
+      fluidRow(
+        box(width = 12, height = NULL,
+            htmlOutput("text"))
+      )
+    )
+  )
